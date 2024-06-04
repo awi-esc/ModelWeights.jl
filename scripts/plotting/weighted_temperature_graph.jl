acb8a7e0b1969ae7abd2e190a7f68658968f581d
Britta Grusdt <bgrusdt@gmail.com>
2024-06-04 08:32:49 +0200

    updated weighted_temperature_graph to include weightedAvg and correct ranges

--- scripts/plotting/weighted_temperature_graph.jl
+++ scripts/plotting/weighted_temperature_graph.jl
@@ -1,6 +1,7 @@
 using NetCDF
 using Statistics
 using CairoMakie
+using Interpolations
 
 include(joinpath(@__DIR__, "..", "..", "src", "load-data-utils.jl"));
 include(joinpath(@__DIR__, "..", "..", "src", "plot-utils.jl"));
@@ -9,30 +10,57 @@ workDir = joinpath(PATH_TO_WORK_DIR, "weighted_temperature_graph", "weighted_tem
 targetDir = joinpath("plots-replicated-with-julia", "weighted_temperature_graph");
 mkpath(targetDir)
 
-path_to_data = joinpath(workDir, "temperature_anomalies.nc")
+path2Data = joinpath(workDir, "temperature_anomalies.nc")
 
 # use ncdump to see the variables contained within the .nc-file.
 #  or show contents like so:
 #ncfile = NetCDF.open(path_to_data)
-temp_anomalies = ncread(path_to_data, "tas");
-size(temp_anomalies)
+tempAnomalies = ncread(path2Data, "tas");
+size(tempAnomalies)
 
-time =  ncread(path_to_data, "time");
+time =  ncread(path2Data, "time");
 size(time)
-
-model_ensemble =  ncread(path_to_data, "model_ensemble");
-size(model_ensemble)
-
-
-unweighted_avg = mean(temp_anomalies, dims = 2);
-size(unweighted_avg)
+modelEnsemble =  ncread(path2Data, "model_ensemble");
+size(modelEnsemble)
+nbModels = length(modelEnsemble);
 
 xticks = 1960:20:2100
-timeInYears = trunc.(Int, 1960 .+ (time ./ 365))
+timeInYears = trunc.(Int, 1960 .+ (time ./ 365));
+
+function getInterpolatedWeightedQuantiles(quantiles, vals, weights=nothing)
+    if isnothing(weights)
+        weights = ones(length(vals));
+    end
+    indicesSorted = sortperm(vals);
+    weightsSorted = weights[indicesSorted];
+    weightedQuantiles = cumsum(weightsSorted) - 0.5 * weightsSorted;
+    weightedQuantiles = reshape(weightedQuantiles, length(weightedQuantiles), 1);
+    weightedQuantiles = (weightedQuantiles .- minimum(weightedQuantiles)) ./ maximum(weightedQuantiles);
+    
+    #interp_linear = Interpolations.linear_interpolation(vec(weightedQuantiles), vals[indicesSorted]);
+    interp_linear = Interpolations.linear_interpolation(vec(weightedQuantiles), 
+                                                        vals[indicesSorted],
+                                                        extrapolation_bc=Line()
+                                                        );
+     
+    return interp_linear(quantiles)
+end
+
+weights = ncread(joinpath(PATH_TO_WORK_DIR, "calculate_weights_climwip", "climwip", "weights.nc"),
+                 "weight");
+
+quantiles = [0.167, 0.833];
+unweightedRanges = [];
+weightedRanges = [];
+for i_row in axes(tempAnomalies, 1)
+    lower, upper = getInterpolatedWeightedQuantiles(quantiles, tempAnomalies[i_row,:], weights);
+    push!(weightedRanges, [lower, upper]);
+    lower, upper = getInterpolatedWeightedQuantiles(quantiles, tempAnomalies[i_row,:]);
+    push!(unweightedRanges, [lower, upper]);
+end
 
 begin 
-    f = Figure();
-    
+    f = Figure(); 
     ax = Axis(f[1,1], 
         xticks = (xticks, string.(xticks)), 
         limits = ((minimum(timeInYears)-10, maximum(timeInYears)+10), (-1, 5)),
@@ -41,19 +69,27 @@ begin
         ylabel = "Temperature anomaly °C"
     );
     # add ranges 
-    range66 = [quantile(row, [0.167, 0.833]) for row in eachrow(temp_anomalies)];
-    lower =  unweighted_avg .- getindex.(range66, 1)
-    upper = unweighted_avg .+ getindex.(range66, 2)
-    band!(ax, timeInYears, vec(lower), vec(upper), color = (:red, 0.2))
+    lowerUnw = getindex.(unweightedRanges, 1);
+    upperUnw = getindex.(unweightedRanges, 2);
+    band!(ax, timeInYears, vec(lowerUnw), vec(upperUnw), color = (:red, 0.2), 
+          label = "Non-weighted 16.7-83.3perc range");
     
-    points = Point2f.(timeInYears, vec(unweighted_avg))
-    
-    for col in 1:size(temp_anomalies)[2]
-        y = temp_anomalies[:, col]  # Extract the column
-        lines!(ax, timeInYears, y, color = :gray70, label = "ensemble members")
+    lowerWeighted = getindex.(weightedRanges, 1);
+    upperWeighted = getindex.(weightedRanges, 2);
+    band!(ax, timeInYears, vec(lowerWeighted), vec(upperWeighted), color = (:green, 0.2), 
+          label = "Weighted 16.7-83.3perc range");
+        
+    # add results for each model model seperately
+    for i_model in 1:nbModels
+        y = tempAnomalies[:, i_model] 
+        lines!(ax, timeInYears, y, color = :gray80, label = "ensemble members")
     end
-    lines!(ax, timeInYears, vec(unweighted_avg), color = :red, label = "Non-weighted mean")
-    
+    unweightedAvg =  mean(tempAnomalies, dims = 2);
+    weightedAvg = sum(repeat(weights', size(tempAnomalies,1), 1) .* tempAnomalies, dims=2);
+    lines!(ax, timeInYears, vec(unweightedAvg), color = :red, label = "Non-weighted mean")
+    lines!(ax, timeInYears, vec(weightedAvg), color = :green, label = "Weighted mean")
+
+
     axislegend(ax, merge = true, position = :lt)
 end
 f
