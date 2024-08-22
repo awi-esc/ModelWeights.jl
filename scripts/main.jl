eddb409d78790647818d28ac56fd57e92cec7c3a
Britta Grusdt <bgrusdt@gmail.com>
2024-08-22 17:26:39 +0200

    updated paths in main-script

--- scripts/main.jl
+++ scripts/main.jl
@@ -7,23 +7,22 @@ using NCDatasets
 
 RUN_ON_ALBEDO = true;
 
-############# Get preprocessed data from ESMValTool ######################
-if RUN_ON_ALBEDO
-    # just use one reference period
-    pathsObsData= Dict("pr" => "/albedo/work/projects/p_forclima/preproc_data_esmvaltool/historical/recipe_historical_pr_20240815_124334/preproc/climatology_historical1/pr");
-    pathsModelData = Dict("pr" => "/albedo/work/projects/p_forclima/preproc_data_esmvaltool/historical/recipe_historical_pr_20240815_124334/preproc/climatology_full/pr");
-else
-    # TODO: recheck these!!
-    pathsObsData = Dict("tp" => "/Users/brgrus001/output-from-albedo/generated_recipe_historical_pr_20240802_115940/preproc/climatologic_diagnostic",
-                        "hur" => "/Users/brgrus001/output-from-albedo/generated_recipe_historical_hur_20240802_120722/preproc/climatologic_diagnostic");
-    pathsModelData = Dict("pr" => "/Users/brgrus001/output-from-albedo/generated_recipe_historical_pr_20240726_112338/preproc/climatologic_diagnostic", 
-                          "hur" => "/Users/brgrus001/output-from-albedo/generated_recipe_historical_hur_20240801_094610/preproc/climatologic_diagnostic");
+############# Get preprocessed data from ESMValTool ######################  
+begin 
+    if RUN_ON_ALBEDO
+        # just use one reference period
+        pathsObsData= Dict("pr" => "/albedo/work/projects/p_forclima/preproc_data_esmvaltool/historical/recipe_historical_pr_20240815_124334/preproc/climatology_historical1/pr");
+        pathsModelData = Dict("pr" => "/albedo/work/projects/p_forclima/preproc_data_esmvaltool/historical/recipe_historical_pr_20240815_124334/preproc/climatology_full/pr");
+    else
+        pathsObsData = Dict("pr" => "/Users/brgrus001/output-from-albedo/recipe_historical_pr_20240815_124334/preproc/climatology_historical1/pr")
+        pathsModelData = Dict("pr" => "/Users/brgrus001/output-from-albedo/recipe_historical_pr_20240815_124334/preproc/climatology_full/pr")
+    end
 end
 
 obsData = SimilarityWeights.loadPreprocData(pathsObsData, ["ERA5"]);
 weightsVars = Dict{String, Number}("pr" => 1, "tas" => 1, "hur" => 1); 
 modelData = SimilarityWeights.loadPreprocData(pathsModelData, ["CMIP"]);
-###########################################
+##########################################################################
 
 # Performance and independent weights
 wP = SimilarityWeights.getPerformanceWeights(modelData, obsData, weightsVars);
