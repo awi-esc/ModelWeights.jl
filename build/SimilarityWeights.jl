9a6a1e403e8c8706d1f8c5a6a672437cbaf7d78d
brgrus001 <bgrusdt@gmail.com>
2024-09-08 03:23:22 +0200

    added build/ of documentation for now

---
+++ build/SimilarityWeights.jl
@@ -0,0 +1,12 @@
+module SimilarityWeights
+
+include("data-utils.jl")
+include("plot-utils.jl")
+include("weights.jl")
+include("plot-weights.jl")
+include("plot-data.jl")
+include("main-functions.jl")
+
+export Config, loadPreprocData, getIndependenceWeights, getPerformanceWeights, getOverallWeights, runWeights
+
+end
