1cd3e0ad7ec9f9b178aa9c66adb9693825cb5eaa
Britta Grusdt <bgrusdt@gmail.com>
2024-12-14 12:16:27 +0100

    updated run climwip script

--- scripts/run-climwip-simplified.jl
+++ scripts/run-climwip-simplified.jl
@@ -3,6 +3,7 @@ import ModelWeights as mw
 using DimensionalData
 using ColorSchemes
 using NCDatasets
+using Statistics
 
 path_data = "/albedo/work/projects/p_forclima/preproc_data_esmvaltool/climwip/climwip-simplified_20241013_073358"; 
 path_configs = "/albedo/home/brgrus001/ModelWeights/configs/climwip_config";
@@ -25,7 +26,6 @@ obs_data = mw.loadDataFromESMValToolConfigs(
 
 # TODO: or load from seperate yaml file:
 # data = mw.loadDataFromYAML(
-
 # )
 
 # Compute Weights
@@ -47,7 +47,8 @@ weights = mw.computeWeights(model_data, obs_data, config_weights);
 # save weights as Julia object
 mw.saveWeightsAsJuliaObj(weights, joinpath(target_dir, fn_jld2))
 mw.saveWeightsAsNCFile(weights, joinpath(target_dir, fn_nc))
-weights = mw.loadWeightsFromJLD2(target_path)
+
+weights = mw.loadWeightsFromJLD2(joinpath(target_dir, fn_jld2));
 weights_all_members = mw.distributeWeightsAcrossMembers(weights.w);
 
 # make some Plots
@@ -69,7 +70,7 @@ sij_var = dropdims(reduce(+, weights.independence_distances, dims=:diagnostic), 
 figs_sij = mw.plotDistancesIndependence(sij_var, dimname="member1");
 
 
-# Apply computed weights - Temperature map plots
+# Climwip Plots - Temperature map plots
 # TODO fix:
 # data_climwip = mw.loadDataFromESMValToolConfigs(
 #     path_data, path_configs;
@@ -89,64 +90,47 @@ data_temp_map_reference = mw.loadDataFromESMValToolConfigs(
 );
                 
 # compute weighted averages and plot results
-data_ref = mw.indexData(data_temp_map_reference, "tas", "CLIM", "weighted_temperature_map_reference")
-data_future = mw.indexData(data_temp_map_future, "tas", "CLIM", "weighted_temperature_map_future")
-# to align with original data
+data_ref = mw.indexData(data_temp_map_reference, "tas", "CLIM", "weighted_temperature_map_reference");
+data_future = mw.indexData(data_temp_map_future, "tas", "CLIM", "weighted_temperature_map_future");
+# just to align with original data
 data_ref = data_ref[lat = Where(x -> x <= 68.75)];
 data_future = data_future[lat = Where(x -> x <= 68.75)];
 
-weighted_ref = mw.computeWeightedAvg(data_ref; weights = weights_all_members)
-weighted_future = mw.computeWeightedAvg(data_future; weights = weights_all_members)
-diff_ww = weighted_future .- weighted_ref;
-diff_ww = mw.sortLongitudesWest2East(diff_ww);
+diff = data_future .- data_ref;
 
-# Weighted mean temp. change 2081-2100 minus 1995-2014
-f1 = mw.plotMeansOnMap(
-    diff_ww, "Weighted mean temp. change 2081-2100 minus 1995-2014";
-    ColorSchemes.Reds.colors
-)
-
-# some sanity checks
+# sanity checks: compare to original data (must have adapted the latitudes above)
 function compareToOrigData(data, data_orig)
-    mat = isapprox.(data, data_orig, atol=10^-2);
+    mat = isapprox.(data, data_orig, atol=10^-4);
     @assert ismissing.(data_orig) == ismissing.(data)
     approxEqual = all(x -> ismissing(x) || x, mat)
     @assert approxEqual
 end
 
+title_f1 = "Weighted mean temp. change 2081-2100 minus 1995-2014";
+weighted_avg = mw.computeWeightedAvg(diff; weights = weights_all_members);
+#weighted_avg = mw.sortLongitudesWest2East(weighted_avg);
+f1 = mw.plotMeansOnMap(weighted_avg, title_f1; ColorSchemes.Reds.colors)
+
+title_f2 = "Weighted minus unweighted mean temp. change: 2081-2100 minus 1995-2014";
+unweighted_avg = mw.computeWeightedAvg(diff; use_members=false);
+#unweighted_avg = mw.sortLongitudesWest2East(unweighted_avg);
+diff_wu = weighted_avg .- unweighted_avg;
+f2 = mw.plotMeansOnMap(diff_wu, title_f2; ColorSchemes.Reds.colors)
+
+
 begin
-    # compare to original data (must have adapted the latitudes above)
     data_orig = NCDataset("/albedo/home/brgrus001/ModelWeights/reproduce-climwip-figs/recipe_climwip_test_basic_data/work/weighted_temperature_map/weighted_temperature_map/temperature_change_weighted_map.nc");
     data_ww_orig = data_orig["__xarray_dataarray_variable__"][:,:];
-    compareToOrigData(diff_ww, data_ww_orig)
-    d = DimArray(data_ww_orig, (Dim{:lon}(Array(dims(diff_ww, :lon))), Dim{:lat}(Array(dims(diff_ww, :lat)))))
-    mw.plotMeansOnMap(d, "Weighted mean temp. change 2081-2100 - 1995-2014"; ColorSchemes.Reds.colors);
+    compareToOrigData(weighted_avg, data_ww_orig)
+    # d = DimArray(data_ww_orig, (Dim{:lon}(Array(dims(weighted_avg, :lon))), Dim{:lat}(Array(dims(weighted_avg, :lat)))))
+    # mw.plotMeansOnMap(d, title_f1 * " (original data)"; ColorSchemes.Reds.colors);
 end
 
-####### DOESNT WORK YET ########
-# Weighted minus unweighted mean temp. change 2081-2100 minus 1995-2014
-data_ref_models = mw.summarizeEnsembleMembersVector(data_ref, true);
-data_future_models = mw.summarizeEnsembleMembersVector(data_future, true);
-
-unweighted_ref = mw.computeWeightedAvg(data_ref);
-unweighted_future = mw.computeWeightedAvg(data_future);
-diff_uu = unweighted_future .- unweighted_ref;
-diff_uu = mw.sortLongitudesWest2East(diff_uu);
-diff_wu = diff_ww .- diff_uu;
-
-f2 = mw.plotMeansOnMap(
-    diff_wu,
-    "Weighted minus unweighted mean temp. change: 2081-2100 minus 1995-2014";
-    ColorSchemes.Reds.colors
-)
-
-# compare to orig data
 begin
     data_orig = NCDataset("/albedo/home/brgrus001/ModelWeights/reproduce-climwip-figs/recipe_climwip_test_basic_data/work/weighted_temperature_map/weighted_temperature_map/temperature_change_difference_map.nc");
     data_wu_orig = data_orig["__xarray_dataarray_variable__"][:,:];
     compareToOrigData(diff_wu, data_wu_orig)
 end
-####### DOESNT WORK YET: results not equal (AssertionError) ########
 
 
 # Apply computed weights - Temperature graph plots
@@ -155,15 +139,16 @@ data_temp_graph = mw.loadDataFromESMValToolConfigs(
     dir_per_var = false,
     subset = mw.Constraint(aliases = ["weighted_temperature_graph"])
 );
-data_graph = mw.indexData(data_temp_graph, "tas", "ANOM", "weighted_temperature_graph")
+data_graph = mw.indexData(data_temp_graph, "tas", "ANOM", "weighted_temperature_graph");
 # TODO: check applyWeights fn and difference!
 # this will compute the weighted avg based on the average across the respective members of each model
 #weighted_avg = mw.applyWeights(data_graph, weights_all_members);
 
+# TODO check if uncertainties are also identical! And weighted/unweighted avgs!
 weighted_avg = mw.computeWeightedAvg(data_graph; weights = weights_all_members);
 unweighted_avg = mw.computeWeightedAvg(data_graph);
-
 uncertainties = mw.getUncertaintyRanges(data_graph, weights_all_members);
+
 f3 = mw.plotTempGraph(
     data_graph, 
     (weighted=weighted_avg, unweighted=unweighted_avg),
@@ -174,7 +159,6 @@ f3 = mw.plotTempGraph(
 tas_orig = NCDataset("/albedo/home/brgrus001/ModelWeights/reproduce-climwip-figs/recipe_climwip_test_basic_data/work/weighted_temperature_graph/weighted_temperature_graph/temperature_anomalies.nc")["tas"];
 @assert tas_orig == data_graph
 
-# TODO check if uncertainties are also identical! And weighted/unweighted avgs!
 
 
 # TODO: Plot ensemble spread for some models with >1 ensemble member
