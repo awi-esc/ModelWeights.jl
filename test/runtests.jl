162cbbb040ee014cfeb77ac22e494dea5c95f5e1
Britta Grusdt <bgrusdt@gmail.com>
2025-05-22 15:21:18 +0200

    use debug logger for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,25 @@
+using Logging
 using ModelWeights
 using Test
 
-@testset "ModelWeights tests" begin
-    @testset "Models tests" begin
-        include("models_tests.jl")
-    end
-    @testset "Data tests" begin
-        include("data_tests.jl")
-    end
-    @testset "Utils tests" begin
-        include("utils_tests.jl")
-    end
-    @testset "Metadata tests" begin
-        include("metadata_tests.jl")
-    end
-    @testset "Weights tests" begin
-        include("weights_tests.jl")
+debug_logger = ConsoleLogger(stderr, Logging.Debug)
+
+with_logger(debug_logger) do
+    @testset "ModelWeights tests" begin
+        @testset "Models tests" begin
+            include("models_tests.jl")
+        end
+        @testset "Data tests" begin
+            include("data_tests.jl")
+        end
+        @testset "Utils tests" begin
+            include("utils_tests.jl")
+        end
+        @testset "Metadata tests" begin
+            include("metadata_tests.jl")
+        end
+        @testset "Weights tests" begin
+            include("weights_tests.jl")
+        end
     end
 end
