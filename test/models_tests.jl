e6750ff672f486b118d7ca19f8c12656b0b336ed
Britta Grusdt <bgrusdt@gmail.com>
2025-06-06 15:35:07 +0200

    updated model tests

--- test/models_tests.jl
+++ test/models_tests.jl
@@ -2,7 +2,7 @@
     realizations = [1,2,3]
     initialization_methods = [2,3,4]
     physics = [1,1,1]
-    member_ids = ModelWeights.buildCMIP5EnsembleMember(realizations, initialization_methods, physics)
+    member_ids = ModelWeights.Data.buildCMIP5EnsembleMember(realizations, initialization_methods, physics)
     @test member_ids == ["r1i2p1", "r2i3p1", "r3i4p1"]
 end
 
